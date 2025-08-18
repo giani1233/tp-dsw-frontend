6180719bf8f4616fb41fa150f9869ca6a2123c2a
Luciano Lapunzina <l.lapunzina@gmail.com>
2025-08-18 20:21:18 -0300

    menu dropdown en header + ruta register

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,13 +12,31 @@ function Header() {
         <>
             <div className='Header'>                
                 <nav className='navigation'>
-                    <a href="http://localhost:5173/" title="Home" className="logo"> <img src="src/images/mainlogo.png" alt="Logo" width="45"/> </a>                    
+                    <a href="/" title="Home" className="logo"> <img src="src/images/mainlogo.png" alt="Logo" width="45"/> </a>                    
                     <span className='menu-icon' onClick={handleMenuClick}>Ver Menú</span>
                     <ul className={menuVisible ? 'show' : ''}>
-                        <li><a href="#">Ciudad</a></li>
-                        <li><a href="#">Categorías</a></li>
+                        <li>
+                            <select>
+                                <option value="noCity">Ciudad</option>
+                                <option value="city1">Córdoba</option>
+                                <option value="city2">La Plata</option>
+                                <option value="city3">Mendoza</option>
+                                <option value="city4">Rosario</option>
+                                <option value="city5">San Juan</option>
+                                <option value="city6">Santa Fe</option>
+                            </select></li> {/* Menu Dropdown */}
+                        <li>
+                            <select>
+                                <option value="noCategory">Categorías</option>
+                                <option value="cat1">Categoría 1</option>
+                                <option value="cat2">Categoría 2</option>
+                                <option value="cat3">Categoría 3</option>
+                                <option value="cat4">Categoría 4</option>
+                                <option value="cat5">Categoría 5</option>
+                                <option value="cat6">Categoría 6</option>
+                            </select></li> {/* Menu Dropdown */}
                         <li><a href="/login">Iniciar Sesión</a></li>
-                        <li><a href="#">Registrarse</a></li>
+                        <li><a href="/register">Registrarse</a></li>
                     </ul>
                     <form> 
                         <input type="search" name="query" placeholder="Buscar"/>
---
+++ src/pages/Register.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+
+
+function Register() {
+    return (
+        <>
+            <Header />
+                <h1>REGISTER</h1>
+            <Footer />
+        </>
+    )
+}
+
+export default Register
